ca89bf6cf2b853cd055196260677a60a7e5184f1
Javier Ponce <imr3ivaj@gmail.com>
2025-11-03 19:11:33 -0600

    feat: integrate PromptInput component into Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
+import {
+  PromptInput,
+  PromptInputBody,
+  PromptInputFooter,
+  PromptInputSubmit,
+  PromptInputTextarea,
+  PromptInputTools,
+} from "@/components/ai-elements/prompt-input";
+// import { Image } from "@/components/ai-elements/image"
+
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Hola mundo</h1>
+    <div>
+      {/* <Image
+        alt="Example generated image"
+        className="aspect-square h-[150px] border"
+      /> */}
+      <PromptInput onSubmit={() => {}} className="mt-4">
+        <PromptInputBody>
+          <PromptInputTextarea />
+        </PromptInputBody>
+        <PromptInputFooter>
+          <PromptInputTools />
+          <PromptInputSubmit />
+        </PromptInputFooter>
+      </PromptInput>
     </div>
   );
 }

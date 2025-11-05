b736cbf99b185df31f469cccaa38ec7fd20dea30
Javier Ponce <imr3ivaj@gmail.com>
2025-11-04 20:02:43 -0600

    feat: add Convex dependency and integrate ConvexClientProvider in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <main className="mx-auto max-w-4xl">
-          {children}
+          <ConvexClientProvider>{children}</ConvexClientProvider>
         </main>
       </body>
     </html>
